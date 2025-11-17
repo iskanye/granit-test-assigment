d1ccf770a2475a8b777ba760160c5be707195eeb
iskanye <iskanyeban@gmail.com>
2025-11-17 22:02:16 +0300

    refactor: program class

--- ProjectsGenerator/Program.cs
+++ ProjectsGenerator/Program.cs
@@ -1,122 +1,128 @@
 ﻿using Microsoft.Data.Sqlite;
 
-const int minInserts = 99;
-const int maxInserts = 2000;
-
-string tableCreationStmt =
-"""
-CREATE TABLE IF NOT EXISTS checks
-(
-    № INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    Контакт_1 TEXT NOT NULL,
-    Контакт_2 TEXT NOT NULL,
-    Тип_проверки TEXT NOT NULL,
-    Объект TEXT NOT NULL,
-    Модификация TEXT NOT NULL,
-    Результат_проверки REAL 
-)
-""";
-
-string tableInsertionStmt =
-"""
-INSERT INTO checks
-(
-    Контакт_1,
-    Контакт_2,
-    Тип_проверки,
-    Объект,
-    Модификация,
-    Результат_проверки 
-) 
-VALUES
-(
-    @contact1,
-    @contact2,
-    @checkType,
-    @object,
-    @modification,
-    @checkResult
-)
-""";
-
-string contacts = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-string[] checkTypes =
-[
-    "напряжения",
-    "сопротивления",
-    "тока",
-];
-
-string[] objects =
-[
-    "ЛОГ",
-    "МОД",
-    "ПРОД",
-    "МИД",
-    "МУР"
-];
-
-Dictionary<string, string[]> modifications = new()
+class Program
 {
-    {"ЛОГ", ["ЛОГ0", "ЛОГ4", "ЛОГ5", "ЛОГ7", "ЛОГ9"]},
-    {"МОД", ["МОД1", "МОД2", "МОД4", "МОД6", "МОД7"]},
-    {"ПРОД", ["ПРОД2", "ПРОД3", "ПРОД6", "ПРОД8", "ПРОД11"]},
-    {"МИД", ["МИД1", "МИД2", "МИД3", "МИД6", "МИД9"]},
-    {"МУР", ["МУР1", "МУР3", "МУР8", "МУР9", "МУР11", "МУР12"]}
-};
+    const int minInserts = 99;
+    const int maxInserts = 2000;
 
-void InitDB(SqliteConnection conn)
-{
-    conn.Open();
+    const string tableCreationStmt =
+    """
+    CREATE TABLE IF NOT EXISTS checks
+    (
+        № INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        Контакт_1 TEXT NOT NULL,
+        Контакт_2 TEXT NOT NULL,
+        Тип_проверки TEXT NOT NULL,
+        Объект TEXT NOT NULL,
+        Модификация TEXT NOT NULL,
+        Результат_проверки REAL 
+    )
+    """;
 
-    // Создание таблицы
-    using var create = new SqliteCommand(tableCreationStmt, conn);
-    create.ExecuteNonQuery();
+    const string tableInsertionStmt =
+    """
+    INSERT INTO checks
+    (
+        Контакт_1,
+        Контакт_2,
+        Тип_проверки,
+        Объект,
+        Модификация,
+        Результат_проверки 
+    ) 
+    VALUES
+    (
+        @contact1,
+        @contact2,
+        @checkType,
+        @object,
+        @modification,
+        @checkResult
+    )
+    """;
 
-    var inserts = Random.Shared.Next(minInserts, maxInserts);
+    const string contacts = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    for (int i = 0; i < inserts; i++)
+    readonly static string[] checkTypes =
+    [
+        "напряжения",
+        "сопротивления",
+        "тока",
+    ];
+
+    readonly static string[] objects =
+    [
+        "ЛОГ",
+        "МОД",
+        "ПРОД",
+        "МИД",
+        "МУР"
+    ];
+
+    readonly static Dictionary<string, string[]> modifications = new()
     {
-        // Подготовка к вставке
-        var contactsArr = contacts.ToArray();
-        Random.Shared.Shuffle(contactsArr);
+        {"ЛОГ", ["ЛОГ0", "ЛОГ4", "ЛОГ5", "ЛОГ7", "ЛОГ9"]},
+        {"МОД", ["МОД1", "МОД2", "МОД4", "МОД6", "МОД7"]},
+        {"ПРОД", ["ПРОД2", "ПРОД3", "ПРОД6", "ПРОД8", "ПРОД11"]},
+        {"МИД", ["МИД1", "МИД2", "МИД3", "МИД6", "МИД9"]},
+        {"МУР", ["МУР1", "МУР3", "МУР8", "МУР9", "МУР11", "МУР12"]}
+    };
 
-        var contact1 = contactsArr[0];
-        var contact2 = contactsArr[1];
-        var checkType = checkTypes[Random.Shared.Next(0, checkTypes.Length)];
-        var checkedObject = objects[Random.Shared.Next(0, objects.Length)];
+    static void InitDB(SqliteConnection conn)
+    {
+        conn.Open();
 
-        var objMods = modifications[checkedObject];
-        var modification = objMods[Random.Shared.Next(1, objMods.Length)];
+        // Создание таблицы
+        using var create = new SqliteCommand(tableCreationStmt, conn);
+        create.ExecuteNonQuery();
 
-        // Вставка
-        using var insert = new SqliteCommand(tableInsertionStmt, conn);
-        insert.Parameters.Add("@contact1", SqliteType.Text).Value = "Контакт " + contact1;
-        insert.Parameters.Add("@contact2", SqliteType.Text).Value = "Контакт " + contact2;
-        insert.Parameters.Add("@checkType", SqliteType.Text).Value = "измерение " + checkType;
-        insert.Parameters.Add("@object", SqliteType.Text).Value = "Объект " + checkedObject;
-        insert.Parameters.Add("@modification", SqliteType.Text).Value = modification;
-        insert.Parameters.Add("@checkResult", SqliteType.Real).Value = Random.Shared.NextDouble() * 1000;
+        var inserts = Random.Shared.Next(minInserts, maxInserts);
 
-        insert.ExecuteNonQuery();
+        for (int i = 0; i < inserts; i++)
+        {
+            // Подготовка к вставке
+            var contactsArr = contacts.ToArray();
+            Random.Shared.Shuffle(contactsArr);
+
+            var contact1 = contactsArr[0];
+            var contact2 = contactsArr[1];
+            var checkType = checkTypes[Random.Shared.Next(0, checkTypes.Length)];
+            var checkedObject = objects[Random.Shared.Next(0, objects.Length)];
+
+            var objMods = modifications[checkedObject];
+            var modification = objMods[Random.Shared.Next(1, objMods.Length)];
+
+            // Вставка
+            using var insert = new SqliteCommand(tableInsertionStmt, conn);
+            insert.Parameters.Add("@contact1", SqliteType.Text).Value = "Контакт " + contact1;
+            insert.Parameters.Add("@contact2", SqliteType.Text).Value = "Контакт " + contact2;
+            insert.Parameters.Add("@checkType", SqliteType.Text).Value = "измерение " + checkType;
+            insert.Parameters.Add("@object", SqliteType.Text).Value = "Объект " + checkedObject;
+            insert.Parameters.Add("@modification", SqliteType.Text).Value = modification;
+            insert.Parameters.Add("@checkResult", SqliteType.Real).Value = Random.Shared.NextDouble() * 1000;
+
+            insert.ExecuteNonQuery();
+        }
+    }
+
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("no args");
+        }
+
+        try
+        {
+            var connectionString = "Data Source=usersdata.db";
+            using var conn = new SqliteConnection(connectionString);
+            InitDB(conn);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error: " + e.ToString());
+        }
+
+        return 0;
     }
 }
-
-if (args.Length == 0)
-{
-    Console.WriteLine("no args");
-}
-
-try
-{
-    var connectionString = "Data Source=usersdata.db";
-    using var conn = new SqliteConnection(connectionString);
-    InitDB(conn);
-}
-catch (Exception e)
-{
-    Console.WriteLine("error: " + e.ToString());
-}
-
-return 0;
